2ad7b5684c80f5e251f72a0271d6e8a47b1f9fde
ProfoundlyParker <parkerramey@yahoo.com>
2023-09-20 16:16:57 -0400

    Optimizing performance & converting to PWA

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,7 @@ import './Card.css';
 const Card = ({ firstName, lastName, email, image }) => {
     return (
         <div className='tc background dib br4 ma2 grow bw2 shadow-5'>
-            <img src={image} height='auto' width='auto' alt="robots"></img>
+            <img src={image} height='300px' width='300px' alt="robots"></img>
          <div>
             <h2>{firstName} {lastName}</h2>
             <p>{email}</p>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { createLogger } from 'redux-logger';
 import  thunkMiddleware  from 'redux-thunk';
 import './index.css';
 import App from './Containers/App';
-import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { searchRobots, getRobotsReducer } from './reducers';
 import * as serviceWorker from './serviceWorker';
@@ -27,9 +26,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 serviceWorker.register();
