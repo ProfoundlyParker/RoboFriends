20a84c3239ad9b5ba2ba22a4467c7c66f3120a8d
ProfoundlyParker <parkerramey@yahoo.com>
2025-01-08 15:35:59 -0500

    Testing CirclCI connection

--- src/Containers/App.test.js
+++ src/Containers/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen, act } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 import App from "./App";
 
 const middlewares = [thunk];
